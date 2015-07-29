d50a8a96898d550847d1babf047ddbc644c218de
Daniel O'Connor <daniel@optimizely.com>
2015-07-29 10:48:29 -0700

    Load the SCSS bundles in a project.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = function() {
-  console.log('Automatically generate documentation for Sass files.')
+var glob = require('glob');
+var shell = require('shelljs');
+
+module.exports = function(base) {
+  console.log('Automatically generate documentation for Sass files.');
+
+  base = base || '';
+
+  options = {
+    cwd: shell.pwd(),
+    debug: true,
+    matchBase: true,
+  }
+
+  var files = glob.sync(base + '**/!(_)*.scss', options);
+  console.log(files);
 }
