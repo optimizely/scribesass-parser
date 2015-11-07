16db7f283181b0a22a6fd9185a97c45001701d82
Daniel O'Connor <daniel@optimizely.com>
2015-11-07 15:57:13 -0800

    Comment out the debugging code.

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ module.exports = {
 }
 
 // For debugging purposes:
-module.exports.create('node_modules/optimizely-lego/src/core/core.scss', function(err, files) {
-  // dd(files[0].path);
-  // dd(module.exports.getScss(files[0].ast));
-  // dd(module.exports.getComments(files[0].ast));
-  // dd(module.exports.getFileProperties(files[0].ast));
-  // dd(module.exports.getGroups(files));
-});
+// module.exports.create('node_modules/optimizely-lego/src/core/core.scss', function(err, files) {
+//   dd(files[0].path);
+//   dd(module.exports.getScss(files[0].ast));
+//   dd(module.exports.getComments(files[0].ast));
+//   dd(module.exports.getFileProperties(files[0].ast));
+//   dd(module.exports.getGroups(files));
+// });
