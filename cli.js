f3ba7a44816f8910fcbcef0df909deae00540d6a
Daniel O'Connor <daniel@danoc.me>
2016-11-14 19:21:57 -0800

    Add a basic CLI.

---
+++ cli.js
@@ -0,0 +1,32 @@
+const commander = require('commander');
+const util = require('util');
+const fs = require('fs');
+const scribesass = require('./index');
+
+
+commander
+  .version('0.0.0')
+  .description('')
+  .usage('[options] <file ...>')
+  .option('-o, --out-file [file]', 'Store the output in a file')
+  .parse(process.argv);
+
+const input = commander.args[0];
+
+
+scribesass.create(input, (err, fileArr) => {
+  fileArr.map((file) => {
+    file.properties = scribesass.getFileProperties(file.ast);
+    delete file.ast;
+  });
+
+  if (commander.outFile) {
+    fs.writeFileSync(commander.outFile, util.inspect(fileArr, { depth: null }));
+  } else {
+    console.log(util.inspect(fileArr, { depth: null }));
+  }
+  // console.log(fileArr);
+  // console.log(scribesass.getFileProperties(fileArr[0].ast));
+  // console.log(scribesass.getComments(fileArr[0].ast));
+  // console.log(util.inspect(fileArr, { depth: null }));
+});
